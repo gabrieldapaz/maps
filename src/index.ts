8f513309f021c64003d1383753686aa43431f985
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-31 17:03:45 -0300

    add google maps api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-import { User } from './User';
-import { Company } from './Company';
-
-const user = new User();
-console.log(user);
-
-const company = new Company();
-console.log(company);
+// import { User } from './User';
+// import { Company } from './Company';
