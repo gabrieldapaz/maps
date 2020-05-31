118acd7bcae45a36b22fe33f4b9e9d2052de59c5
gabrieldapaz <mgpp@cin.ufpe.br>
2020-05-31 17:20:40 -0300

    add method for create an user marker on the map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,14 +1,26 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: {
         lat: 0,
-        lng: 0
-      }
+        lng: 0,
+      },
+    });
+  }
+  // A class can be used as a class and as a type
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
     });
   }
 }
-
